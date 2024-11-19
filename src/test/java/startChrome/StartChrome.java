6c98705943a8cb9ebfff1a88b687da0ca1ffefc9
migena.nosto <migena.nosto@teamviewer.com>
2024-11-19 13:26:56 +0200

    7 prwta challenges

--- src/test/java/startChrome/StartChrome.java
+++ src/test/java/startChrome/StartChrome.java
@@ -1,6 +1,9 @@
 package startChrome;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
@@ -9,7 +12,9 @@ import java.util.logging.Logger;
 
 public class StartChrome {
     /** Add the url of the page you want to do actions in. */
-    private static final String URL_TO_OPEN = "WHERE_TO?";
+    private static final String URL_TO_OPEN = "https://teamviewer.atlassian.net/jira";
+
+
 
     @Test
     public static void openCloseDriver(){
@@ -28,17 +33,39 @@ public class StartChrome {
         driver.manage().window().maximize();
 
         /** Add your actions here (README links for help) */
+        //driver.findElement(By.className("css-1ah6ujv")).click();   //TV logo
+
+        //driver.findElement(By.className("css-khjbnv")).click();   //Create group
+
+        //driver.findElement(By.xpath("//button[@data-testid='atlassian-navigation--create-button']")).click();
+        //driver.findElement(locators("atlassian-navigation--create-button")).click();
+
+        //By createButtonLocator = By.xpath("//button[@data-testid='atlassian-navigation--create-button']");
+        //WebElement createButton = driver.findElement(createButtonLocator);
+
+        //createButton.click();
+
+        driver.findElement(By.xpath("//*[@data-test-id='search-dialog-input']")).click();
+        WebElement searchButton = driver.findElement(locators("search-dialog-input"));
+        searchButton.click();
+        searchButton.sendKeys("TEAM-60387" + Keys.ENTER);
 
 
         /** End of your actions */
 
-        closeChrome(driver);
+        //closeChrome(driver);
     }
 
     /** Can the crhome close method run even if the test fails and exits? *
      * HINT: TestNG structure                                             */
-    private static void closeChrome(WebDriver driver){
+   private static void closeChrome(WebDriver driver){
         driver.close();
         driver.quit();
     }
+
+    private static By locators(String dataTestID){
+        return By.xpath("//*[@data-testid='"+dataTestID+"']");
+
+    }
+
 }
