42a30489825004bfe25120058b2c862c818a4c2b
Migena Nosto <migena.nosto@teamviewer.com>
2024-11-22 13:29:21 +0200

    main

--- src/test/java/startChrome/StartChrome.java
+++ src/test/java/startChrome/StartChrome.java
@@ -10,11 +10,9 @@ import org.testng.annotations.Test;
 
 import java.util.logging.Logger;
 
-public class StartChrome {
+public class StartChrome{
     /** Add the url of the page you want to do actions in. */
     private static final String URL_TO_OPEN = "https://teamviewer.atlassian.net/jira";
-
-
 
     @Test
     public static void openCloseDriver(){
@@ -33,27 +31,10 @@ public class StartChrome {
         driver.manage().window().maximize();
 
         /** Add your actions here (README links for help) */
-        //driver.findElement(By.className("css-1ah6ujv")).click();   //TV logo
-
-        //driver.findElement(By.className("css-khjbnv")).click();   //Create group
-
-        //driver.findElement(By.xpath("//button[@data-testid='atlassian-navigation--create-button']")).click();
-        //driver.findElement(locators("atlassian-navigation--create-button")).click();
-
-        //By createButtonLocator = By.xpath("//button[@data-testid='atlassian-navigation--create-button']");
-        //WebElement createButton = driver.findElement(createButtonLocator);
-
-        //createButton.click();
-
-        driver.findElement(By.xpath("//*[@data-test-id='search-dialog-input']")).click();
-        WebElement searchButton = driver.findElement(locators("search-dialog-input"));
-        searchButton.click();
-        searchButton.sendKeys("TEAM-60387" + Keys.ENTER);
-
 
         /** End of your actions */
 
-        //closeChrome(driver);
+        closeChrome(driver);
     }
 
     /** Can the crhome close method run even if the test fails and exits? *
@@ -64,8 +45,7 @@ public class StartChrome {
     }
 
     private static By locators(String dataTestID){
-        return By.xpath("//*[@data-testid='"+dataTestID+"']");
+        return By.xpath("//*[@data-test-id='"+dataTestID+"']");
 
     }
-
 }
